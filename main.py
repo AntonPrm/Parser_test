c24bca46af52a5ec0e1bc2af7b6eebb2e5fcef91
Anton <kondakow90@gmail.com>
2021-09-11 13:43:36 +0500

    update lib

--- main.py
+++ main.py
@@ -1,16 +1,28 @@
 import requests
 from bs4 import BeautifulSoup
+from pprint import pprint
+import pandas as pd
+import numpy as np
 
-
-url = 'https://quotes.toscrape.com/'
+url = 'https://api.hh.ru/vacancies?industry=7&per_page=1&page=0'
+#url = 'https://quotes.toscrape.com/'
 
 def main():
     response = requests.get(url)
-    soup = BeautifulSoup(response.text, 'lxml')
-    quotes = soup.find_all('span', class_='text')
+    #pprint(response.json())
 
-    for quote in quotes:
-        print(quote.text)
+
+    vacancy_id = response.json()['items'][0]['alternate_url'].replace('https://hh.ru/vacancy/','')
+    vacancy_url = 'https://api.hh.ru/vacancies/' + vacancy_id
+    request = requests.get(vacancy_url)
+    pprint(request.json())
+
+
+#soup = BeautifulSoup(response.text, 'lxml')
+    #quotes = soup.find_all('span', class_='text')
+
+    #for quote in quotes:
+    #    print(quote.text)
 
 
 if __name__ == '__main__':
